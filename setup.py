8c63327b8814bdd3499c1696b5a5f0eb9fe7fc76
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-12-05 12:14:46 +0900

    Update version: v0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dxx",
-    version="0.1.2",
+    version="0.1.3",
     description="dxx is a library for io and converting audio files with a .DXX extension.",
     packages=["dxx"],
     install_requires=open('requirements.txt').read().splitlines(),
